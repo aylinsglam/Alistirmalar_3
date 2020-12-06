bf7fdf5f1540cca26cf9a3cceaabb1f3a93810fc
aylinsglam <73175714+aylinsglam@users.noreply.github.com>
2020-12-06 14:37:17 +0300

    Add files via upload

---
+++ exchange_sort.py
@@ -0,0 +1,9 @@
+liste=[7,3,5,8,2,9,4,15,6]
+n=len(liste)
+for i in range(0,n-1):
+    for j in range(i+1,n):
+        if liste[j]<liste[i]:
+            temp=liste[i]
+            liste[i]=liste[j]
+            liste[j]=temp
+    print(liste)
